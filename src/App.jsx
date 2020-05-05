eb0d04fdc43cf78721b86e3f2eb53d628de41157
Olexiy Sokurenko <ol.sakura@gmail.com>
2020-05-05 10:03:16 +0200

    Error handling sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,50 @@
-import React, { Component } from 'react';
-import CardList from './components/CardList';
-import SearchBox from './components/SearchBox';
+import React, { Component } from "react";
+import CardList from "./components/CardList";
+import SearchBox from "./components/SearchBox";
 import Scroll from "./components/Scroll";
-import './App.css';
-
+import "./App.css";
+import ErrorBoundres from "./components/ErrorBoundry";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchField: '',
+      searchField: "",
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange = (event) => {
-    this.setState({searchField:event.target.value });
-  }
+    this.setState({ searchField: event.target.value });
+  };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robots) => robots.name.toLowerCase().includes(this.state.searchField.toLowerCase()));
-    if(this.state.robots.length===0){
-        return <h1>Loading...</h1>
+    const filteredRobots = this.state.robots.filter((robots) =>
+      robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundres>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundres>
+          </Scroll>
+        </div>
+      );
     }
-    else{
-        return (
-            <div className="tc">
-              <h1 className="f1">RoboFriends</h1>
-              <SearchBox searchChange={this.onSearchChange} />
-              <Scroll>
-                <CardList robots={filteredRobots} />
-              </Scroll>
-            </div>
-          );
-    }
-
   }
 }
 
---
+++ src/components/ErrorBoundry.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oooops. This is not good</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundry;
