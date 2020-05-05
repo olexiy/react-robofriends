090e8ce0576515e5277cf248d107116788dbad09
Olexiy Sokurenko <ol.sakura@gmail.com>
2020-05-05 10:28:51 +0200

    Updates

---
+++ precache-manifest.a22cf64caa2b8c8fac2d5c6461c1d7fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa02cf2ac2cc0b121de7a064a4e00fb",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/react-robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "f0bb118964b07f01c3c7",
+    "url": "/react-robofriends/static/css/main.e8d7e5a6.chunk.css"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/react-robofriends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robofriends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bb118964b07f01c3c7",
+    "url": "/react-robofriends/static/js/main.60f0da27.chunk.js"
+  },
+  {
+    "revision": "91e8b2a67cd29869f165",
+    "url": "/react-robofriends/static/js/runtime-main.aef47920.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
